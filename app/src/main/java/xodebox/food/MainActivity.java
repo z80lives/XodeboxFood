d673de53bbacb29e72bb36b905d37f5141c6ebec
shath <shath.ibrahim@gmail.com>
2016-05-28 09:15:31 +0800

    Changed the MainActivity Layout file.

--- app/src/main/java/xodebox/food/MainActivity.java
+++ app/src/main/java/xodebox/food/MainActivity.java
@@ -1,13 +1,13 @@
 package xodebox.food;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +18,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+       Button fab = (Button) findViewById(R.id.hButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "AOK!", Toast.LENGTH_SHORT).show();
+                /*
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                        */
             }
         });
     }
