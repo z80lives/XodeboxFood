328ddaebc16036fa38a7b262ad840aa8c41d1319
shath <shath.ibrahim@gmail.com>
2016-05-28 10:07:06 +0800

    Implemented the pullout menu system for main and added a blank activity for Restaurant Page.

--- app/src/main/java/xodebox/food/MainActivity.java
+++ app/src/main/java/xodebox/food/MainActivity.java
@@ -1,5 +1,6 @@
 package xodebox.food;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -23,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "AOK!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, RestaurantPage.class));
                 /*
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
---
+++ app/src/main/java/xodebox/food/RestaurantPage.java
@@ -0,0 +1,13 @@
+package xodebox.food;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestaurantPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_page);
+    }
+}
