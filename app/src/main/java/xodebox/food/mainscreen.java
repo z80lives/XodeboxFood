cb5a0af2cab8c9f36798b45886c3627ff7e84ce6
shath <shath.ibrahim@gmail.com>
2016-06-18 11:00:16 +0800

    Merge remote-tracking branch 'upstream/branch1' into branch1-unstable
    # Conflicts:
    #	.idea/gradle.xml
    #	app/build.gradle
    #	app/google-services.json
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/xodebox/food/LoginActivity.java
    #	app/src/main/java/xodebox/food/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/layout/content_main.xml
    #	app/src/main/res/menu/menu_main.xml
    #	app/src/main/res/values/strings.xml
    #	build.gradle

--- app/src/main/java/xodebox/food/mainscreen.java
+++ app/src/main/java/xodebox/food/mainscreen.java
@@ -1,6 +1,5 @@
 package xodebox.food;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
