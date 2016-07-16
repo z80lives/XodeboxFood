2137c7d8df45e6fbd6d87c7ae31f126c28ae69c4
tmaxletitgo <tmaxletitgo@gmail.com>
2016-07-16 17:42:25 +0800

    added config file

---
+++ app/src/main/java/xodebox/food/ui/Configurations.java
@@ -0,0 +1,12 @@
+package xodebox.food.ui;
+
+import android.content.SharedPreferences;
+
+/**
+ * Created by Max on 16/7/2016.
+ */
+public abstract class Configurations {
+
+    public static final String datapref="XodeboxPref";
+
+}
