7080b8592974dcd3a52da6a442f849c8b844f3f8
shath <shath.ibrahim@gmail.com>
2016-08-01 20:12:03 +0800

    Modified restaurant layout file and collection layout file. Collection Layout: Changed List View to Recycler View.

--- app/src/main/java/xodebox/food/ui/fragments/CollectionsScreenFragment.java
+++ app/src/main/java/xodebox/food/ui/fragments/CollectionsScreenFragment.java
@@ -4,16 +4,19 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 import xodebox.food.R;
+import xodebox.food.common.models.Model;
 
 /**
  * TODO: Replace listView with RecyclerView. Read and create views from JSON/XML File. Refactor and clean the code.
@@ -24,7 +27,7 @@ import xodebox.food.R;
 public class CollectionsScreenFragment extends Fragment {
     private static final String TAG = "CollectionsScreen";
     private View rootView;
-    private ListView listView;
+    private RecyclerView listView;
     private Context context;
 
     /** {@inheritDoc} **/
@@ -47,7 +50,7 @@ public class CollectionsScreenFragment extends Fragment {
      * Fetch views from layout and store them in the class variable.
      */
     private boolean fetchViews(){
-        listView = (ListView) rootView.findViewById(R.id.collection_item_list);
+        listView = (RecyclerView) rootView.findViewById(R.id.collection_item_list);
         if(listView == null)
             return  false;
         return true;
@@ -70,12 +73,56 @@ public class CollectionsScreenFragment extends Fragment {
 
         //Create and add few items into our list
         String[] names = {"Chalk and cheese", "Olive Garden", "Pizza Hut", "KGB", "Ali Maju", "Nandos"};
+        List<Model> models = new ArrayList<>();
+        models.add(new Model());
+        models.add(new Model());
 
+        //prepare recycler view
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(context);
+        listView.setLayoutManager(layoutManager);//Throws Null pointer exception
+        listView.setItemAnimator(new DefaultItemAnimator());
 
         //A temporary adapter for our list view
-        ArrayAdapter<String> collectionsAdapter = new ArrayAdapter<>(rootView.getContext(), R.layout.collection_item, R.id.name, Arrays.asList(names) );
-
+       // ArrayAdapter<String> collectionsAdapter = new ArrayAdapter<>(rootView.getContext(), R.layout.collection_item, R.id.name, Arrays.asList(names) );
+        RecyclerView.Adapter collectionsAdapter = new CollectionRecyclerAdapter(models);
         //Set the adapter to the view
         listView.setAdapter(collectionsAdapter);
+    }
+}
+
+class CollectionRecyclerAdapter extends RecyclerView.Adapter <CollectionRecyclerAdapter.ViewHolder> {
+    private List<Model> itemList;
+
+    public CollectionRecyclerAdapter(List<Model> itemList) {
+        super();
+        this.itemList = itemList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.collection_item, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+    //    onBindViewHolder(holder, position);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position, List<Object> payloads) {
+        super.onBindViewHolder(holder, position, payloads);
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
     }
 }
